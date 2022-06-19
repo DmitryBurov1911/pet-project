8472df5ffc4a979d72a73d139fa8971d9817411c
Dmitry <99758072+DmitryBurov1911@users.noreply.github.com>
2022-06-20 00:47:38 +0300

    Update forms.py

--- coolsite/men/forms.py
+++ coolsite/men/forms.py
@@ -43,22 +43,6 @@ class RegisterUserForm(UserCreationForm):
         model = User
         fields = ("username", "email", "password1", "password2")
 
-        widgets = {
-            "username": forms.TextInput(attrs={
-                'class': 'form-input'
-            }), 
-            "email": forms.EmailInput(attrs={
-                'class': 'form-input'
-            }), 
-            "password1": forms.PasswordInput(attrs={
-                'class': 'form-input'
-            }), 
-            "password2": forms.PasswordInput(attrs={
-                'class': 'form-input'
-            }), 
-
-        }
-
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
